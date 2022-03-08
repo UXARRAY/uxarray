f9b0815828261975180c3a31c897b05ed3fff576
Rajeev Jain <rajeeja@gmail.com>
2022-03-08 16:18:57 -0600

    o Fix docs

--- uxarray/helpers.py
+++ uxarray/helpers.py
@@ -1,70 +1,70 @@
 import xarray as xr
 from pathlib import PurePath
 
+
 # helper function to find file type
 def determine_file_type(filepath):
-   """Checks file path and contents to determine file type. Supports
-   detection of UGrid, SCRIP, Exodus and shape file.
-   
-   Parameters: string, required
-      Filepath of the file for which the filetype is to be determined.
+    """Checks file path and contents to determine file type. Supports detection
+    of UGrid, SCRIP, Exodus and shape file.
 
-   Returns: string
-      File type: ug, exo, scrip or shp
+    Parameters: string, required
+       Filepath of the file for which the filetype is to be determined.
 
-   Raises:
-      RuntimeError: Invalid file type
-   """
-   msg = ""
-   # exodus with coord
-   try:
-      # extract the file name and extension
-      path = PurePath(filepath)
-      file_extension = path.suffix
+    Returns: string
+       File type: ug, exo, scrip or shp
 
-      # try to open file with xarray and test for exodus
-      ext_ds = xr.open_dataset(filepath, mask_and_scale=False)["coord"]
-      mesh_filetype = "exo"
-   except KeyError as e:
-      # exodus with coordx
-      try:
-            ext_ds = xr.open_dataset(filepath,
-                                    mask_and_scale=False)["coordx"]
+    Raises:
+       RuntimeError: Invalid file type
+    """
+    msg = ""
+    # exodus with coord
+    try:
+        # extract the file name and extension
+        path = PurePath(filepath)
+        file_extension = path.suffix
+
+        # try to open file with xarray and test for exodus
+        ext_ds = xr.open_dataset(filepath, mask_and_scale=False)["coord"]
+        mesh_filetype = "exo"
+    except KeyError as e:
+        # exodus with coordx
+        try:
+            ext_ds = xr.open_dataset(filepath, mask_and_scale=False)["coordx"]
             mesh_filetype = "exo"
-      except KeyError as e:
+        except KeyError as e:
             # scrip with grid_center_lon
             try:
-               ext_ds = xr.open_dataset(
-                  filepath, mask_and_scale=False)["grid_center_lon"]
-               mesh_filetype = "scrip"
+                ext_ds = xr.open_dataset(
+                    filepath, mask_and_scale=False)["grid_center_lon"]
+                mesh_filetype = "scrip"
             except KeyError as e:
-               # ugrid with Mesh2
-               try:
-                  ext_ds = xr.open_dataset(filepath,
+                # ugrid with Mesh2
+                try:
+                    ext_ds = xr.open_dataset(filepath,
                                              mask_and_scale=False)["Mesh2"]
-                  mesh_filetype = "ugrid"
-               except KeyError as e:
-                  print("This is not a supported NetCDF file")
-   except (TypeError, AttributeError) as e:
-      msg = str(e) + ': {}'.format(filepath)
-   except (RuntimeError, OSError) as e:
-      # check if this is a shp file
-      # we won't use xarray to load that file
-      if file_extension == ".shp":
+                    mesh_filetype = "ugrid"
+                except KeyError as e:
+                    print("This is not a supported NetCDF file")
+    except (TypeError, AttributeError) as e:
+        msg = str(e) + ': {}'.format(filepath)
+    except (RuntimeError, OSError) as e:
+        # check if this is a shp file
+        # we won't use xarray to load that file
+        if file_extension == ".shp":
             mesh_filetype = "shp"
-      else:
+        else:
             msg = str(e) + ': {}'.format(filepath)
-   except ValueError as e:
-      # check if this is a shp file
-      # we won't use xarray to load that file
-      if file_extension == ".shp":
+    except ValueError as e:
+        # check if this is a shp file
+        # we won't use xarray to load that file
+        if file_extension == ".shp":
             mesh_filetype = "shp"
-      else:
+        else:
             msg = str(e) + ': {}'.format(filepath)
-   finally:
-      if (msg != ""):
+    finally:
+        if (msg != ""):
             msg = str(e) + ': {}'.format(filepath)
             print(msg)
             raise RuntimeError(msg)
 
-   return mesh_filetype
+    return mesh_filetype
