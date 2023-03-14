cf4aae4e975136443e79d131712c9edb205e46c3
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2023-03-14 00:59:08 -0500

    dual-mesh implementation, more robust test cases

--- test/test_mpas.py
+++ test/test_mpas.py
@@ -1,4 +1,4 @@
-from uxarray._mpas import _read_mpas, _add_fill_values
+from uxarray._mpas import _read_mpas, _add_fill_values, _primal_to_ugrid, _dual_to_ugrid
 import uxarray as ux
 import xarray as xr
 from unittest import TestCase
@@ -12,26 +12,38 @@ current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 class TestMPAS(TestCase):
     """Test suite for Read MPAS functionality."""
 
+    # sample mpas dataset
     mpas_grid_path = current_path / 'meshfiles' / "mpas" / "QU" / 'mesh.QU.1920km.151026.nc'
+    mpas_xr_ds = xr.open_dataset(mpas_grid_path)
 
-    # fill value
+    # fill value (remove once there is a unified approach in uxarray)
     int_dtype = np.uint32
     fv = np.iinfo(int_dtype).max
 
+    def test_primal_to_ugrid_execution(self):
+        """Tests execution of _primal_to_ugrid()"""
+        ugrid_ds = xr.Dataset()
+        _primal_to_ugrid(self.mpas_xr_ds, ugrid_ds)
+
+    def test_dual_to_ugrid_execution(self):
+        """Tests execution of _dual_to_ugrid()"""
+        ugrid_ds = xr.Dataset()
+        _dual_to_ugrid(self.mpas_xr_ds, ugrid_ds)
+
     def test_read_mpas(self):
         """Tests execution of _read_mpas()"""
-        mpas_xr_ds = xr.open_dataset(self.mpas_grid_path)
-        mpas_ugrid = _read_mpas(mpas_xr_ds)
+        mpas_primal_ugrid = _read_mpas(self.mpas_xr_ds, use_dual=False)
+        mpas_dual_ugrid = _read_mpas(self.mpas_xr_ds, use_dual=True)
 
-    def test_mpas_grid(self):
+    def test_mpas_to_grid(self):
         """Tests creation of Grid object from converted MPAS dataset."""
-        mpas_xr_ds = xr.open_dataset(self.mpas_grid_path)
-        mpas_uxgrid = ux.Grid(mpas_xr_ds)
+        mpas_uxgrid = ux.Grid(self.mpas_xr_ds)
 
-    def test_mpas_ugrid_conversion(self):
-        """Verifies that the conversion to UGRID was successful."""
-        mpas_xr_ds = xr.open_dataset(self.mpas_grid_path)
-        ds = _read_mpas(mpas_xr_ds)
+    def test_primal_to_ugrid_conversion(self):
+        """Verifies that the Primal-Mesh was converted properly."""
+
+        # primal-mesh encoded in the UGRID conventions
+        ds = _read_mpas(self.mpas_xr_ds, use_dual=False)
 
         # check for correct dimensions
         expected_ugrid_dims = [
@@ -53,12 +65,35 @@ class TestMPAS(TestCase):
         # check for zero-indexing
         assert ds['Mesh2_face_nodes'].min() == 0
 
+    def test_dual_to_ugrid_conversion(self):
+        """Verifies that the Dual-Mesh was converted properly."""
+
+        # dual-mesh encoded in the UGRID conventions
+        ds = _read_mpas(self.mpas_xr_ds, use_dual=True)
+
+        # check for correct dimensions
+        expected_ugrid_dims = ['nMesh2_node', "nMesh2_face", "Three"]
+        for dim in expected_ugrid_dims:
+            assert dim in ds.sizes
+
+        # check for correct length of coordinates
+        assert len(ds['Mesh2_node_x']) == len(ds['Mesh2_node_y'])
+        assert len(ds['Mesh2_face_x']) == len(ds['Mesh2_face_y'])
+
+        # check for correct shape of face nodes
+        nMesh2_face = ds.sizes['nMesh2_face']
+        assert ds['Mesh2_face_nodes'].shape == (nMesh2_face, 3)
+
+        # check for zero-indexing
+        assert ds['Mesh2_face_nodes'].min() == 0
+
     def test_add_fill_values(self):
-        """Test _add_fill_values() implementation, output should be both zero-
-        indexed and padded values should be replaced with fill values."""
+        """Test _add_fill_values() implementation, output should be both be
+        zero-indexed and padded values should be replaced with fill values."""
 
         # two cells with 2 and 3 padded faces respectively
         test_verticesOnCell = np.array([[1, 2, 1, 1], [3, 4, 5, 3]])
+
         # cell has 2 and 3 nodes respectively
         test_nEdgesOnCell = np.array([2, 3])
 
--- uxarray/_mpas.py
+++ uxarray/_mpas.py
@@ -1,27 +1,25 @@
 import xarray as xr
 import numpy as np
-from pathlib import PurePath
-from datetime import datetime
 
+# remove/edit once unified fill value approach is implemented
 int_dtype = np.uint32
 fill_val = np.iinfo(int_dtype).max
 
 
-def _to_ugrid(in_ds, out_ds):
-    """If input dataset (``in_ds``) file is an unstructured MPAS file, function
-    will reassign MPAS variables to UGRID conventions in output file
-    (``out_ds``).
+def _primal_to_ugrid(in_ds, out_ds):
+    """If input dataset is an unstructured MPAS file, function will encode the
+    MPAS Primal-Mesh in the UGRID conventions.
 
     Parameters
     ----------
     in_ds : xarray.Dataset
-        Original MPAS dataset of interest being used
-
+        Input MPAS dataset
     out_ds : xarray.Dataset
-        Output dataset encoded in the UGRID conventions
+        Output dataset where the MPAS Primal-Mesh is encoded in the UGRID
+        conventions
     """
 
-    # corners of primal mesh cells (in degrees)
+    # corners of primal-mesh cells (in degrees)
     lonVertex = np.rad2deg(in_ds['lonVertex'].values)
     latVertex = np.rad2deg(in_ds['latVertex'].values)
 
@@ -43,7 +41,7 @@ def _to_ugrid(in_ds, out_ds):
             "units": "degrees_north",
         })
 
-    # centers of primal mesh cells (in degrees)
+    # centers of primal-mesh cells (in degrees)
     lonCell = np.rad2deg(in_ds['lonCell'].values)
     latCell = np.rad2deg(in_ds['latCell'].values)
 
@@ -65,10 +63,10 @@ def _to_ugrid(in_ds, out_ds):
             "units": "degrees_north",
         })
 
-    # vertex indices that surround each primal mesh cell
+    # vertex indices that surround each primal-mesh cell
     verticesOnCell = in_ds['verticesOnCell'].values
 
-    # max number of edges & vertices that make up each cell
+    # max number of edges & vertices that make up each primal-mesh cell
     nEdgesOnCell = in_ds['nEdgesOnCell'].values
 
     # correct to be zero-indexed and to use fill values
@@ -83,26 +81,113 @@ def _to_ugrid(in_ds, out_ds):
             "start_index": int_dtype(0)
         })
 
-    # add Mesh2_edge_nodes if available
-    if "verticesOnEdge" in in_ds:
-        # vertex indices that saddle a given edge
-        verticesOnEdge = in_ds['verticesOnEdge'].values
+    # vertex indices that saddle a given edge
+    verticesOnEdge = in_ds['verticesOnEdge'].values
 
-        # convert to zero-indexed
+    # convert to zero-indexed
+    if verticesOnEdge.min() == 1:
         verticesOnEdge -= 1
 
-        out_ds["Mesh2_edge_nodes"] = xr.DataArray(
-            data=verticesOnEdge,
-            dims=["nMesh2_edge", "Two"],
-            attrs={
-                "cf_role": "edge_node_connectivity",
-                "start_index": int_dtype(0)
-            })
+    out_ds["Mesh2_edge_nodes"] = xr.DataArray(
+        data=verticesOnEdge,
+        dims=["nMesh2_edge", "Two"],
+        attrs={
+            "cf_role": "edge_node_connectivity",
+            "start_index": int_dtype(0)
+        })
+
+
+def _dual_to_ugrid(in_ds, out_ds):
+    """If input dataset is an unstructured MPAS file, function will encode the
+    MPAS Dual-Mesh in the UGRID conventions.
+
+    Parameters
+    ----------
+    in_ds : xarray.Dataset
+        Input MPAS dataset
+    out_ds : xarray.Dataset
+        Output dataset where the MPAS Dual-Mesh is encoded in the UGRID
+        conventions
+    """
+
+    # corners of dual-mesh cells (in degrees)
+    lonCell = np.rad2deg(in_ds['lonCell'].values)
+    latCell = np.rad2deg(in_ds['latCell'].values)
+
+    out_ds['Mesh2_node_x'] = xr.DataArray(
+        lonCell,
+        dims=["nMesh2_node"],
+        attrs={
+            "standard_name": "longitude",
+            "long_name": "longitude of mesh nodes",
+            "units": "degrees_east",
+        })
+
+    out_ds['Mesh2_node_y'] = xr.DataArray(
+        latCell,
+        dims=["nMesh2_node"],
+        attrs={
+            "standard_name": "latitude",
+            "long_name": "latitude of mesh nodes",
+            "units": "degrees_north",
+        })
+
+    # centers of dual-mesh cells (in degrees)
+    lonVertex = np.rad2deg(in_ds['lonVertex'].values)
+    latVertex = np.rad2deg(in_ds['latVertex'].values)
+
+    out_ds['Mesh2_face_x'] = xr.DataArray(
+        lonVertex,
+        dims=["nMesh2_face"],
+        attrs={
+            "standard_name": "longitude",
+            "long_name": "longitude of center nodes",
+            "units": "degrees_east",
+        })
+
+    out_ds['Mesh2_face_y'] = xr.DataArray(
+        latVertex,
+        dims=["nMesh2_face"],
+        attrs={
+            "standard_name": "latitude",
+            "long_name": "latitude of center nodes",
+            "units": "degrees_north",
+        })
+
+    # vertex indices that surround each dual-mesh cell
+    cellsOnVertex = in_ds['cellsOnVertex'].values
+
+    # convert to zero-indexed
+    if cellsOnVertex.min() == 1:
+        cellsOnVertex -= 1
+
+    out_ds["Mesh2_face_nodes"] = xr.DataArray(
+        data=cellsOnVertex,
+        dims=["nMesh2_face", "Three"],
+        attrs={
+            "cf_role": "face_node_connectivity",
+            "start_index": int_dtype(0)
+        })
+
+    # vertex indices that saddle a given edge
+    cellsOnEdge = in_ds['cellsOnEdge'].values
+
+    # convert to zero-indexed
+    if cellsOnEdge.min() == 1:
+        cellsOnEdge -= 1
+
+    out_ds["Mesh2_edge_nodes"] = xr.DataArray(
+        data=cellsOnEdge,
+        dims=["nMesh2_edge", "Two"],
+        attrs={
+            "cf_role": "edge_node_connectivity",
+            "start_index": int_dtype(0)
+        })
 
 
 def _add_fill_values(verticesOnCell, nEdgesOnCell):
-    """Corrects the input (``verticesOnCell``) to be zero-indexed and to use
-    fill-values instead of padding / zero-fills.
+    """Corrects the input verticesOnCell to be zero-indexed and to use fill-
+    values instead of padding / zero-fills.
 
     Parameters
     ----------
@@ -119,36 +204,50 @@ def _add_fill_values(verticesOnCell, nEdgesOnCell):
     # max vertices/edges per cell
     maxEdges = verticesOnCell.shape[1]
 
+    # is verticesOnCell one-indexed
+    one_idx = [True if verticesOnCell.min() == 0 else False]
+
     # iterate over the maximum number of vertices on a cell
     for vert_idx in range(maxEdges):
         # mask for non-padded values
         mask = vert_idx < nEdgesOnCell
         # convert non-padded values to zero-index
-        verticesOnCell[mask, vert_idx] -= 1
+        if one_idx:
+            verticesOnCell[mask, vert_idx] -= 1
         # replace remaining padding or zeros with fill_value
         verticesOnCell[np.logical_not(mask), vert_idx] = fill_val
 
     return verticesOnCell
 
 
-def _read_mpas(ext_ds):
-    """Function to read in a MPAS dataset and encode the Primal mesh in the
-    UGRID conventions.
+def _read_mpas(ext_ds, use_dual=False):
+    """Function to read in a MPAS Grid dataset and encode either the Primal or
+    Dual mesh in the UGRID conventions.
 
     Parameters
     ----------
     ext_ds : xarray.Dataset, required
         MPAS datafile of interest
+    use_dual : bool
+        Flag to select whether to encode the Dual-Mesh
 
     Returns
     -------
     ds : xarray.Dataset
         ugrid aware :class:`xarray.Dataset`
     """
+
+    # empty dataset that will contain our encoded MPAS mesh
     ds = xr.Dataset()
 
     try:
-        _to_ugrid(ext_ds, ds)
+        # convert dual-mesh to UGRID
+        if use_dual:
+            _dual_to_ugrid(ext_ds, ds)
+        # convert primal-mesh to UGRID
+        else:
+            _primal_to_ugrid(ext_ds, ds)
+
         ds["Mesh2"] = xr.DataArray(
             attrs={
                 "cf_role": "mesh_topology",
