9cd3a91b61ef90393fd406e7e831b3dab45a763b
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2023-04-20 17:27:08 -0600

    test structure

---
+++ test/test_plot.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+from unittest import TestCase
+
+current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+class TestPlot(TestCase):
+
+    def test_example(self):
+        pass
