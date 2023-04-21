61258c5b93f24f6730e4742036bc6876334464f0
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2023-04-20 18:04:13 -0600

    antimeridian crossing and helper work

--- test/test_plot.py
+++ test/test_plot.py
@@ -2,10 +2,19 @@ import os
 from pathlib import Path
 from unittest import TestCase
 
+import xarray as xr
+import uxarray as ux
+
+from uxarray.plot.helpers import compute_antimeridian_crossing
+
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
 class TestPlot(TestCase):
+    ug_filename1 = current_path / "meshfiles" / 'ugrid' / "geoflow-small" / "grid.nc"
+    grid1_ds = xr.open_dataset(ug_filename1)
+    grid1 = ux.Grid(grid1_ds)
 
-    def test_example(self):
+    def test_compute_antimeridian_crossing(self):
+        compute_antimeridian_crossing(self.grid1)
         pass
--- uxarray/plot/helpers.py
+++ uxarray/plot/helpers.py
@@ -1,2 +1,56 @@
-def helper():
-    return None
+import uxarray
+
+from uxarray.constants import INT_FILL_VALUE
+from uxarray.helpers import close_face_nodes
+
+
+def closed_face_verts(grid: uxarray.Grid):
+
+    x = grid.Mesh2_node_x.values
+    y = grid.Mesh2_node_y.values
+    face_dimension = grid.Mesh2_face_dimension.values
+
+    closed_face_nodes = close_face_nodes(grid.Mesh2_face_nodes.values,
+                                         grid.nMesh2_face,
+                                         grid.nMaxMesh2_face_nodes)
+    face_verts_x = []
+    face_verts_y = []
+    for face_idx, face_dim in enumerate(face_dimension):
+        face_verts_x.append(x[closed_face_nodes[face_idx, 0:face_dim]])
+        face_verts_y.append(y[closed_face_nodes[face_idx, 0:face_dim]])
+
+    return face_verts_x, face_verts_y
+
+
+def compute_antimeridian_crossing(grid: uxarray.Grid, threshold=180.0):
+    """Locates any face that crosses the antimeridian."""
+
+    face_verts_x, face_verts_y = closed_face_verts(grid)
+
+    # for vert_x, vert_y in zip(face_verts_x, face_verts_y):
+
+    return
+
+    # longitude (x) values of each face
+    # face_nodes = self.Mesh2_face_nodes.astype(np.int32).values
+    # face_lon = self.Mesh2_node_x.values[face_nodes]
+
+    # pad last value to properly represent each side
+    face_lon_pad = np.pad(face_lon, (0, 1), 'wrap')[:-1]
+
+    # magnitude of each side
+    side_diff = np.abs(np.diff(face_lon_pad))
+
+    # true for any node that is part of an edge that crosses antimeridian
+    crossed_mask_2d = side_diff > threshold
+
+    # true for any face that contains a node that crosses antimeridian
+    crossed_mask_1d = np.any(crossed_mask_2d, axis=1)
+
+    # indices of faces that cross antimeridian
+    crossed_indices = tuple(np.argwhere(crossed_mask_1d).squeeze())
+
+    # include only faces that cross antimeridian
+    crossed_mask_2d = crossed_mask_2d[crossed_indices, :].squeeze()
+
+    return crossed_indices, crossed_mask_2d
