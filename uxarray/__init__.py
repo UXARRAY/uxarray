80245224bbd48c0aed0198ec7302e9d9fa972332
aazedwick <aazedwick@gmail.com>
2023-05-31 16:10:50 -0500

    Addressed changes requested from anissa111

--- uxarray/__init__.py
+++ uxarray/__init__.py
@@ -1,19 +1,14 @@
 from .grid import *
 from .helpers import *
+# Sets the version of uxarray currently installed
+# Attempt to import the needed modules
+try:
+    from importlib.metadata import version as version
+except ImportError:
+    from importlib_metadata import version as version
 
-
-def __version__():
-    """Returns the version of uxarray currently installed."""
-    # Attempt to import the needed modules
-    try:
-        from importlib.metadata import version as version
-    except ImportError:
-        from importlib_metadata import version as version
-
-    try:
-        __version = version("uxarray")
-    except NameError:
-        # Placeholder version incase an error occurs, such as the library isn't installed
-        __version = "0000"
-
-    return __version
+try:
+    __version = version("uxarray")
+except Exception:
+    # Placeholder version incase an error occurs, such as the library isn't installed
+    __version = "999"
