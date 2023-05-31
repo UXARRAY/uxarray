9d7401a15a91481f74c6310d283a37d90b63c553
aazedwick <aazedwick@gmail.com>
2023-05-31 14:56:00 -0500

    Added __version__ method

--- uxarray/__init__.py
+++ uxarray/__init__.py
@@ -1,2 +1,17 @@
 from .grid import *
 from .helpers import *
+
+
+def __version__():
+    # Attempt to import the needed modules
+    try:
+        from importlib.metadata import version as version
+    except ImportError:
+        from importlib_metadata import version as version
+
+    try:
+        __version = version("uxarray")
+        print(__version)
+    except AttributeError:
+        # Incase something happens, such as the library isn't installed
+        __version = "0000"
