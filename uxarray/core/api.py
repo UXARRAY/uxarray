46355db0000e3e35789a5b2a19c3c2a4f1b37d7a
aazedwick <aazedwick@gmail.com>
2023-07-19 10:56:11 -0500

    Fixed api.py

--- uxarray/core/api.py
+++ uxarray/core/api.py
@@ -272,7 +272,7 @@ def enable_jit_cache():
     code doesn't need to regenerate the machine code every run time. Our
     use case here was to study performance, in regular usage one might
     never turn off caching as it will only help if frequently modifying
-    the code or because users have very limited disk space. The default 
+    the code or because users have very limited disk space. The default
     is on (True)
     """
     uxarray.utils.constants.ENABLE_JIT_CACHE = True
@@ -286,7 +286,7 @@ def disable_jit_cache():
     code doesn't need to regenerate the machine code every run time. Our
     use case here was to study performance, in regular usage one might
     never turn off caching as it will only help if frequently modifying
-    the code or because users have very limited disk space. The default 
+    the code or because users have very limited disk space. The default
     is on (True)
     """
     uxarray.utils.constants.ENABLE_JIT_CACHE = False
