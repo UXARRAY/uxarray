9fd0797f1b539682e73d042ed46968a5a3bc3ae1
philipc2 <philipchmielowiec@gmail.com>
2023-09-18 13:40:00 -0500

    precommit

--- test/test_integrate.py
+++ test/test_integrate.py
@@ -14,7 +14,6 @@ class TestIntegrate(TestCase):
     gridfile_ne30 = current_path / "meshfiles" / "ugrid" / "outCSne30" / "outCSne30.ug"
     dsfile_var2_ne30 = current_path / "meshfiles" / "ugrid" / "outCSne30" / "outCSne30_var2.nc"
 
-
     def test_single_dim(self):
         uxgrid = ux.open_grid(self.gridfile_ne30)
 
@@ -22,12 +21,14 @@ class TestIntegrate(TestCase):
 
         dims = {"nMesh2_face": uxgrid.nMesh2_face}
 
-        uxda = ux.UxDataArray(data=test_data, dims=dims, uxgrid=uxgrid, name='var2')
+        uxda = ux.UxDataArray(data=test_data,
+                              dims=dims,
+                              uxgrid=uxgrid,
+                              name='var2')
 
         integral = uxda.integrate()
 
         pass
-
 
     def test_multi_dim(self):
         uxgrid = ux.open_grid(self.gridfile_ne30)
@@ -36,7 +37,10 @@ class TestIntegrate(TestCase):
 
         dims = {"a": 5, "b": 5, "nMesh2_face": uxgrid.nMesh2_face}
 
-        uxda = ux.UxDataArray(data=test_data, dims=dims, uxgrid=uxgrid, name='var2')
+        uxda = ux.UxDataArray(data=test_data,
+                              dims=dims,
+                              uxgrid=uxgrid,
+                              name='var2')
 
         integral = uxda.integrate()
         pass
