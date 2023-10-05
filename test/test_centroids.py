aae62d44b421dc2ed94ccc586abbae74681e44bc
philipc2 <philipchmielowiec@gmail.com>
2023-10-05 14:52:57 -0500

    add test vs SCRIP centroids

--- test/test_centroids.py
+++ test/test_centroids.py
@@ -1,11 +1,14 @@
 import os
 from unittest import TestCase
 import numpy as np
+import numpy.testing as nt
 import uxarray as ux
 from pathlib import Path
 from uxarray.grid.coordinates import _populate_centroid_coord
 
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
+gridfile_CSne8 = current_path / "meshfiles" / "scrip" / "outCSne8" / "outCSne8.nc"
 
 
 class TestCentroids(TestCase):
@@ -25,3 +28,20 @@ class TestCentroids(TestCase):
         self.assertEqual(expected_centroid[0], grid.Mesh2_face_cart_x)
         self.assertEqual(expected_centroid[1], grid.Mesh2_face_cart_y)
         self.assertEqual(expected_centroid[2], grid.Mesh2_face_cart_z)
+
+    def test_centroids_from_mean_verts_scrip(self):
+        """Test computed centroid values compared to values from a SCRIP
+        dataset."""
+
+        uxgrid = ux.open_grid(gridfile_CSne8)
+
+        expected_face_x = uxgrid.Mesh2_face_x.values
+        expected_face_y = uxgrid.Mesh2_face_y.values
+
+        _populate_centroid_coord(uxgrid, repopulate=True)
+
+        computed_face_x = uxgrid.Mesh2_face_x.values
+        computed_face_y = uxgrid.Mesh2_face_y.values
+
+        nt.assert_array_almost_equal(expected_face_x, computed_face_x)
+        nt.assert_array_almost_equal(expected_face_y, computed_face_y)
