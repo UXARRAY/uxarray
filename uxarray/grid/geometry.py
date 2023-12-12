101a2bc688b0012ea1f504d0fef88000b448f0d6
Philip Chmielowiec <philipchmielowiec@gmail.com>
2023-12-12 09:12:26 -0800

    fix tests

--- uxarray/grid/geometry.py
+++ uxarray/grid/geometry.py
@@ -34,7 +34,7 @@ def _pad_closed_face_nodes(face_node_connectivity, n_face, n_max_face_nodes,
     closed[:, :-1] = face_node_connectivity.copy()
 
     if face_node_connectivity.shape[0] == 1:
-        closed[0, n_nodes_per_face:] = closed[0, 0]
+        closed[0, int(n_nodes_per_face):] = closed[0, 0]
     else:
         for i, final_node_idx in enumerate(n_nodes_per_face):
             closed[i, final_node_idx:] = closed[i, 0]
