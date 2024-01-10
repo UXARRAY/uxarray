dd4ce456cdf862dccce637b8b2486b6cb56b4990
erogluorhan <erogluorhan@gmail.com>
2024-01-09 23:31:22 -0700

    Remove numba from upstream testing as its wheel building fails

--- ci/install-upstream.sh
+++ ci/install-upstream.sh
@@ -43,6 +43,5 @@ python -m pip install \
     git+https://github.com/dask/distributed.git \
     git+https://github.com/aleaxit/gmpy.git \
     git+https://github.com/holoviz/holoviews.git \
-    git+https://github.com/numba/numba.git \
     git+https://github.com/shapely/shapely.git \
     git+https://github.com/holoviz/spatialpandas.git
