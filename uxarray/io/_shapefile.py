39a16fd0b8ea57c74a8d86ce12436bd8b1a406ff
Rajeev Jain <rajeeja@gmail.com>
2024-01-10 13:55:42 -0600

    o start on #635

--- uxarray/io/_shapefile.py
+++ uxarray/io/_shapefile.py
@@ -1,10 +1,11 @@
 from warnings import warn
-
+import geopandas as gpd
 
 def _read_shpfile(filepath):
-    """Read shape file.
+    """Read shape file, use geopandas.
 
-    Not implemented.
+    Parameters: xarray.Dataset, required
+    Returns: ugrid aware xarray.Dataset
     """
     raise RuntimeError(
         "Function not implemented yet. FYI, attempted to read SHAPE file: " +
