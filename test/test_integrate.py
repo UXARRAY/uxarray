698be250f8275b7e8c45a127904552da252ffb74
Hongyu Chen <hyvchen@ucdavis.edu>
2024-01-13 18:52:18 -0800

    try fixing testcases

--- test/test_integrate.py
+++ test/test_integrate.py
@@ -320,7 +320,7 @@ class TestFaceWeights(TestCase):
         face_2 = [[0.5 * np.pi, 0.25 * np.pi], [0.5 * np.pi, 0.0], [np.pi, 0.0],
                   [np.pi, 0.25 * np.pi]]
         face_3 = [[1.2 * np.pi, 0.25 * np.pi], [1.2 * np.pi, 0.0],
-                  [1.6 * np.pi, -0.25 * np.pi], [1.6 * np.pi, 0.25 * np.pi]]
+                  [1.6 * np.pi, -0.01 * np.pi], [1.6 * np.pi, 0.25 * np.pi]]
 
         # Convert the face vertices to xyz coordinates
         face_0 = [node_lonlat_rad_to_xyz(v) for v in face_0]
@@ -351,7 +351,7 @@ class TestFaceWeights(TestCase):
                                         [0.4 * np.pi, 0.5 * np.pi]])
         face_2_latlon_bound = np.array([[0.0, 0.25 * np.pi],
                                         [0.5 * np.pi, np.pi]])
-        face_3_latlon_bound = np.array([[-0.5 * np.pi, 0.25 * np.pi],
+        face_3_latlon_bound = np.array([[-0.01 * np.pi, 0.25 * np.pi],
                                         [1.2 * np.pi, 1.6 * np.pi]])
 
         latlon_bounds = np.array([
