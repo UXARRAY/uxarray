9d711890ee9afb95f4bea248816cae3f6b9f8868
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-01-31 01:20:30 -0600

    docstring

--- uxarray/io/_esmf.py
+++ uxarray/io/_esmf.py
@@ -4,19 +4,25 @@ import xarray as xr
 from uxarray.constants import INT_DTYPE, INT_FILL_VALUE
 
 
-def _read_esmf(ext_ds):
-    """TODO:"""
+def _read_esmf(in_ds):
+    """Function to read in an ESMF Grid dataset and encodes it in the UGRID
+    conventions.
+
+    Adheres to the ESMF Unstructrued Grid Format (ESMFMESH) outlined in the ESMF documentation:
+    https://earthsystemmodeling.org/docs/release/latest/ESMF_refdoc/node3.html#SECTION03028200000000000000
+
+    Parameters
+    ----------
+    in_ds: xr.Dataset
+        ESMF Grid Dataset
+
+    Returns
+    out_ds: xr.Dataset
+        ESMF Grid encoder in the UGRID conventions
+    """
 
     out_ds = xr.Dataset()
 
-    source_dims_dict = _to_ugrid(ext_ds, out_ds)
-
-    return out_ds, source_dims_dict
-
-
-def _to_ugrid(in_ds, out_ds):
-    """TODO:"""
-    # dimensions
     source_dims_dict = {
         "n_node": "nodeCount",
         "n_face": "elementCount",
@@ -25,33 +31,50 @@ def _to_ugrid(in_ds, out_ds):
 
     if in_ds["nodeCoords"].units == "degrees":
         # Spherical Coordinates (in degrees)
-        node_lon = in_ds["nodeCoords"].isel(coordDim=0)
-        node_lat = in_ds["nodeCoords"].isel(coordDim=1)
+        node_lon = in_ds["nodeCoords"].isel(coordDim=0).values
+        node_lat = in_ds["nodeCoords"].isel(coordDim=1).values
 
-        face_lon = in_ds["centerCoords"].isel(coordDim=0)
-        face_lat = in_ds["centerCoords"].isel(coordDim=1)
+        face_lon = in_ds["centerCoords"].isel(coordDim=0).values
+        face_lat = in_ds["centerCoords"].isel(coordDim=1).values
 
         out_ds["node_lon"] = xr.DataArray(
-            data=node_lon.values,
-            dims=[
-                "n_node",
-            ],
+            node_lon,
+            dims=["n_node"],
+            attrs={
+                "standard_name": "longitude",
+                "long_name": "longitude of mesh nodes",
+                "units": "degrees_east",
+            },
         )
 
         out_ds["node_lat"] = xr.DataArray(
-            data=node_lat.values,
-            dims=[
-                "n_node",
-            ],
+            node_lat,
+            dims=["n_node"],
+            attrs={
+                "standard_name": "latitude",
+                "long_name": "latitude of mesh nodes",
+                "units": "degrees_north",
+            },
         )
 
-        out_ds["face_lon"] = xr.DataArray(data=face_lon.values, dims=["n_face"])
+        out_ds["face_lon"] = xr.DataArray(
+            face_lon,
+            dims=["n_face"],
+            attrs={
+                "standard_name": "longitude",
+                "long_name": "longitude of center nodes",
+                "units": "degrees_east",
+            },
+        )
 
         out_ds["face_lat"] = xr.DataArray(
-            data=face_lat.values,
-            dims=[
-                "n_face",
-            ],
+            face_lat,
+            dims=["n_face"],
+            attrs={
+                "standard_name": "latitude",
+                "long_name": "latitude of center nodes",
+                "units": "degrees_north",
+            },
         )
 
     else:
@@ -60,6 +83,12 @@ def _to_ugrid(in_ds, out_ds):
 
     n_nodes_per_face = in_ds["numElementConn"].values
 
+    out_ds["n_nodes_per_face"] = xr.DataArray(
+        data=n_nodes_per_face,
+        dims=["n_face"],
+        attrs={"long_name": "number of non-fill value nodes for each face"},
+    )
+
     if "start_index" in in_ds["elementConn"]:
         start_index = in_ds["elementConn"].start_index
     else:
@@ -67,6 +96,7 @@ def _to_ugrid(in_ds, out_ds):
         start_index = 1
 
     face_node_connectivity = in_ds["elementConn"].values.astype(INT_DTYPE)
+
     for i, max_nodes in enumerate(n_nodes_per_face):
         # convert to zero index and standardize fill values
         face_node_connectivity[i, 0:max_nodes] -= start_index
@@ -75,7 +105,11 @@ def _to_ugrid(in_ds, out_ds):
     out_ds["face_node_connectivity"] = xr.DataArray(
         data=face_node_connectivity,
         dims=["n_face", "n_nodes_per_face"],
-        attrs={"_FillValue": INT_FILL_VALUE},
+        attrs={
+            "cf_role": "face_node_connectivity",
+            "_FillValue": INT_FILL_VALUE,
+            "start_index": INT_DTYPE(0),
+        },
     )
 
-    return source_dims_dict
+    return out_ds, source_dims_dict
