de5d7975e9a84e0b1dd1db4661ebd0dafa5dd2d4
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-01-31 18:13:14 -0600

    test grid and data files together

--- test/test_esmf.py
+++ test/test_esmf.py
@@ -1,19 +1,14 @@
-from uxarray.io._mpas import _replace_padding, _replace_zeros, _to_zero_index
-from uxarray.io._mpas import _read_mpas
 import uxarray as ux
-import xarray as xr
-from unittest import TestCase
-import numpy as np
-import numpy.testing as nt
+
 import os
 from pathlib import Path
 
-from uxarray.constants import INT_DTYPE, INT_FILL_VALUE
 
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
-esmf_ne30_grid_path = current_path / 'meshfiles' / "esmf" / "ne30" / "ne30pg3_ESMFmesh.nc"
+esmf_ne30_grid_path = current_path / 'meshfiles' / "esmf" / "ne30" / "ne30pg3.grid.nc"
+esmf_ne30_data_path = current_path / 'meshfiles' / "esmf" / "ne30" / "ne30pg3.data.nc"
 
 
 
@@ -38,3 +33,15 @@ def test_read_esmf():
 
     for conn in conns:
         assert conn in uxgrid._ds
+
+def test_read_esmf_dataset():
+    """Tests the constructing of a UxDataset from an ESMF Grid and Data
+    File."""
+
+    uxds = ux.open_dataset(esmf_ne30_grid_path, esmf_ne30_data_path)
+
+
+    dims = ['n_node', 'n_face']
+
+    for dim in dims:
+        assert dim in uxds.dims
