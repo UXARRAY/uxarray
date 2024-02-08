12c2d777212849768533605aaca54e61c5aa295e
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-02-08 02:11:40 -0600

    fix file path

--- benchmarks/quad_hexagon.py
+++ benchmarks/quad_hexagon.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import uxarray as ux
 
-current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+current_path = Path(os.path.dirname(os.path.realpath(__file__))).parent[0]
 
 grid_path = current_path / "test" / "meshfiles" / "ugrid" / "quad-hexagon" / "grid.nc"
 data_path = current_path / "test" / "meshfiles" / "ugrid" / "quad-hexagon" / "data.nc"
