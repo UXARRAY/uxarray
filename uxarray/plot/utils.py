f27503703c89ddda0d870075bcfa7376f4b22661
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-03-22 14:51:20 -0500

    add docstring and check for supported backends

--- uxarray/plot/utils.py
+++ uxarray/plot/utils.py
@@ -2,11 +2,20 @@ import holoviews as hv
 
 
 class HoloviewsBackend:
+    """Utility class to compare and set holoviews backends for
+    visualization."""
+
     def __init__(self):
         self.backend = None
 
     def compare_and_set(self, backend: str):
-        """TODO:"""
+        """Compares the currently set holoviews backend and sets it to the
+        desired one, if different."""
+
+        if backend not in ["bokeh", "matplotlib"]:
+            raise ValueError(
+                f"Unsupported backend. Expected one of ['bokeh', 'matplotlib'], but received {backend}"
+            )
 
         if backend != self.backend:
             hv.extension(backend)
