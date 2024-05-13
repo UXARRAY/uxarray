786f221f0450a3fbbecf27a1d79b19c739349e0c
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-05-13 14:40:33 -0500

    update zonal mean boilerplate

--- uxarray/core/zonal.py
+++ uxarray/core/zonal.py
@@ -1,1 +1,19 @@
-# can implement zonal average and other helper functions necessary here
+import numpy as np
+
+
+def _non_conservative_zonal_mean_constant_latitude(
+    data: np.ndarray, lat: float
+) -> np.ndarray:
+    # consider that the data being fed into this function may have multiple non-grid dimensions
+    # (i.e. (time, level, n_face)
+    # this shouldn't lead to any issues, but need to make sure that once you get to the point
+    # where you obtain the indices of the faces you will be using for the zonal average, the indexing
+    # is done properly along the final dimensions
+    # i.e. data[..., face_indicies_at_constant_lat]
+
+    # TODO: obtain indices of the faces we will be considering for the zonal mean
+
+    # the returned array should have the same leading dimensions and a final dimension of one, indicating the mean
+    # (i.e. (time, level, 1)
+
+    return None
