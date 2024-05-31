939b3b44f7fccfb36d1ab4200b6db9205a1f8b6f
Rajeev Jain <rajeeja@gmail.com>
2024-05-31 16:07:38 -0500

    o Add a test mesh file

--- test/test_shpfile.py
+++ test/test_shpfile.py
@@ -12,9 +12,7 @@ current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 class TestShpfile(TestCase):
 
-    # shp_filename = current_path / "meshfiles" / "shp" / "cb_2018_us_nation_20m.shp"
-    # shp_filename = "/Users/mbook/Downloads/boundaries_comm_area_chicago/geo_export_f1c20b37-f84d-4a5f-9452-f0d52104af02.shp"
-    shp_filename = "/Users/mbook/Downloads/peoria/peoria.shp"
+    shp_filename = current_path / "meshfiles" / "shp" / "cb_2018_us_nation_20m"/"cb_2018_us_nation_20m.shp"
 
     def test_read_shpfile(self):
         """Read a shapefile."""
