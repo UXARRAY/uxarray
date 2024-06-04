Possible that np.intp could have different ranges on 32-bit versus 64-bit systems. Using np.int32 for padding, this might fix the overflow error on windows machine: https://github.com/UXARRAY/uxarray/actions/runs/9357045842/job/25755876561\?pr\=808\#step:8:87

uxarray/io/_shapefile.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import xarray as xr
 import numpy as np
 from uxarray.conventions import ugrid
-from uxarray.constants import INT_FILL_VALUE, INT_DTYPE
+from uxarray.constants import INT_FILL_VALUE
 
 
 def _read_shpfile(filepath):
@@ -20,7 +20,7 @@ def _read_shpfile(filepath):
     # Initialize as an empty numpy array
     node_lon = np.array([])
     node_lat = np.array([])
-    connectivity = np.empty((0, max_coord_size), dtype=INT_DTYPE)
+    connectivity = np.empty((0, max_coord_size), dtype=np.int32)
 
     node_index = 0
 