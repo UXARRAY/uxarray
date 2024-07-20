c05cfdadfa7bd4e39a1621fa6f7142752a4763a6
Rajeev Jain <rajeeja@gmail.com>
2024-07-20 04:02:54 -0500

    o Add test for multipolygon case and also test a file

--- test/test_shpfile.py
+++ test/test_shpfile.py
@@ -12,11 +12,24 @@ current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 class TestShpfile(TestCase):
 
-    # shp_filename = current_path / "meshfiles" / "shp" / "cb_2018_us_nation_20m"/"cb_2018_us_nation_20m.shp"
-    shp_filename = current_path / "meshfiles" / "shp" / "5poly/polygons.shp"
+    shp_filename = current_path / "meshfiles" / "shp" / "cb_2018_us_nation_20m"/"cb_2018_us_nation_20m.shp"
+    shp_filename_5poly = current_path / "meshfiles" / "shp" / "5poly/5poly.shp"
+    shp_filename_multi = current_path / "meshfiles" / "shp" / "multipoly/multipoly.shp"
 
     def test_read_shpfile(self):
         """Read a shapefile."""
 
         uxgrid = ux.Grid.from_shapefile(self.shp_filename)
+        assert(uxgrid.validate())
+
+    def test_read_shpfile_multi(self):
+        """Read a shapefile."""
+
+        uxgrid = ux.Grid.from_shapefile(self.shp_filename_multi)
+        assert(uxgrid.validate())
+
+    def test_read_shpfile_5poly(self):
+        """Read a shapefile."""
+
+        uxgrid = ux.Grid.from_shapefile(self.shp_filename_5poly)
         assert(uxgrid.validate())
