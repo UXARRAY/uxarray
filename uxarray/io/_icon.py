ebb528a6a027bda5154e1f948faa2db83ba23bd3
Philip Chmielowiec <philipchmielowiec@gmail.com>
2024-07-31 17:12:06 -0500

    add comments

--- uxarray/io/_icon.py
+++ uxarray/io/_icon.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 def _primal_to_ugrid(in_ds, out_ds):
+    """Encodes the Primal Mesh of an ICON Grid into the UGRID conventions."""
     source_dims_dict = {"vertex": "n_node", "edge": "n_edge", "cell": "n_face"}
 
     # rename dimensions to match ugrid conventions
@@ -78,9 +79,10 @@ def _primal_to_ugrid(in_ds, out_ds):
 
 
 def _read_icon(ext_ds, use_dual=False):
+    """Reads and encodes an ICON mesh into the UGRID conventions."""
     out_ds = xr.Dataset()
 
     if not use_dual:
         return _primal_to_ugrid(ext_ds, out_ds)
     else:
-        raise ValueError
+        raise ValueError("Conversion of the ICON Dual mesh is not yet supported.")
