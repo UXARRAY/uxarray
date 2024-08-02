fdb4e9010e0372ed81f9b30de1e15fa2bb9e3f21
Aaron Zedwick <aazedwick@gmail.com>
2024-08-01 22:15:26 -0500

    Updated cmaps.py

--- uxarray/cmaps.py
+++ uxarray/cmaps.py
@@ -2,25 +2,18 @@ from matplotlib.colors import LinearSegmentedColormap
 
 
 # UXarray themed sequential color map
-def sequential():
-    sequential_map = LinearSegmentedColormap.from_list(
-        "sequential", ((0.000, (0.698, 0.463, 0.231)), (1.000, (0.200, 0.235, 0.125)))
-    )
-
-    return sequential_map
-
+sequential = LinearSegmentedColormap.from_list(
+    "sequential", ((0.000, (0.698, 0.463, 0.231)), (1.000, (0.200, 0.235, 0.125)))
+)
 
 # UXarray themed diverging color map
-def diverging():
-    diverging_map = LinearSegmentedColormap.from_list(
-        "diverging",
-        (
-            (0.000, (0.698, 0.463, 0.231)),
-            (0.400, (0.867, 0.678, 0.498)),
-            (0.500, (1.000, 1.000, 1.000)),
-            (0.600, (0.757, 0.859, 0.541)),
-            (1.000, (0.200, 0.235, 0.125)),
-        ),
-    )
-
-    return diverging_map
+diverging = LinearSegmentedColormap.from_list(
+    "diverging",
+    (
+        (0.000, (0.698, 0.463, 0.231)),
+        (0.400, (0.867, 0.678, 0.498)),
+        (0.500, (1.000, 1.000, 1.000)),
+        (0.600, (0.757, 0.859, 0.541)),
+        (1.000, (0.200, 0.235, 0.125)),
+    ),
+)
