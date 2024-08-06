dbb935c3ca1dd491f00e7b8fe11ca99f932d5e80
Aaron Zedwick <aazedwick@gmail.com>
2024-08-06 16:32:19 -0500

    Updated face construction and ordering

--- uxarray/grid/dual.py
+++ uxarray/grid/dual.py
@@ -1,5 +1,4 @@
 import numpy as np
-import uxarray as ux
 from numba import njit
 from uxarray.constants import INT_FILL_VALUE, INT_DTYPE
 
@@ -12,8 +11,6 @@ def construct_dual(grid):
 
     node_face_connectivity = grid.node_face_connectivity.values
 
-    # Array to hold the final face connectivity information
-    final_faces = []
     for i in range(grid.n_node):
         # Get the number of edges, which is just how many faces are in the node_face
         # TODO: outside of the loop with a vectorized implementation (1 sum call)
@@ -27,10 +24,11 @@ def construct_dual(grid):
             [[INT_FILL_VALUE, INT_FILL_VALUE] for _ in range(n_edges)],
             dtype=INT_DTYPE,
         )
-        _temp_face_edges = np.array(
-            [[INT_FILL_VALUE, INT_FILL_VALUE] for _ in range(n_edges)],
-            dtype=INT_DTYPE,
-        )
+        # _temp_face_edges = np.array(
+        #     [[INT_FILL_VALUE, INT_FILL_VALUE] for _ in range(n_edges)],
+        #     dtype=INT_DTYPE,
+        # )
+        temp_face = np.array([INT_FILL_VALUE for _ in range(n_edges)], dtype=INT_DTYPE)
 
         valid_node_indices = node_face_connectivity[i][0:n_edges]
 
@@ -39,23 +37,24 @@ def construct_dual(grid):
         # Connect the face centers around the node to make dual face
         for node_idx in valid_node_indices:
             # face_temp.set_node(index, node_idx)
-            _set_node(_temp_face_edges, index, node_idx)
+            temp_face[index] = node_idx
+            # _set_node(_temp_face_edges, index, node_idx)
             index += 1
 
         # Order the nodes using the angles so the faces have nodes in counter-clockwise sequence
         node_central = np.array([grid.node_x[i], grid.node_y[i], grid.node_z[i]])
         node_0 = np.array(
             [
-                dual_node_x[_temp_face_edges[0][0]],
-                dual_node_y[_temp_face_edges[0][0]],
-                dual_node_z[_temp_face_edges[0][0]],
+                dual_node_x[temp_face[0]],
+                dual_node_y[temp_face[0]],
+                dual_node_z[temp_face[0]],
             ]
         )
 
-        if _temp_face_edges[0][0] is not INT_FILL_VALUE:
+        if temp_face[0] is not INT_FILL_VALUE:
             _face = _order_nodes(
                 _face_edges,
-                _temp_face_edges,
+                temp_face,
                 node_0,
                 node_central,
                 n_edges,
@@ -63,43 +62,33 @@ def construct_dual(grid):
                 dual_node_y,
                 dual_node_z,
             )
-            final_faces.append(_face)
-
-    # Empty array to hold `node_face_connectivity`
-    node_face_connectivity = np.full(
-        (len(final_faces), grid.n_max_node_faces),
-        INT_FILL_VALUE,
-        dtype=ux.INT_DTYPE,
-    )
-
-    # Populate the node_face array
-    for i, face in enumerate(final_faces):
-        faces = [
-            edge[0] for edge in face
-        ]  # Extract the first element of each edge tuple
-        node_face_connectivity[i, : len(faces)] = (
-            faces  # Assign the faces to the node_face array
-        )
-
+            node_face_connectivity[i] = _face
+            # final_faces.append(_face)
     return node_face_connectivity
 
-
-def _populate(node_face_connectivity, final_faces):
-    for i, face in enumerate(final_faces):
-        faces = [
-            edge[0] for edge in face
-        ]  # Extract the first element of each edge tuple
-        node_face_connectivity[i, : len(faces)] = (
-            faces  # Assign the faces to the node_face array
-        )
-
-    return node_face_connectivity
+    # # Empty array to hold `node_face_connectivity`
+    # node_face_connectivity = np.full(
+    #     (len(final_faces), grid.n_max_node_faces),
+    #     INT_FILL_VALUE,
+    #     dtype=ux.INT_DTYPE,
+    # )
+    #
+    # # Populate the node_face array
+    # for i, face in enumerate(final_faces):
+    #     faces = [
+    #         edge[0] for edge in face
+    #     ]  # Extract the first element of each edge tuple
+    #     node_face_connectivity[i, : len(faces)] = (
+    #         faces  # Assign the faces to the node_face array
+    #     )
+    #
+    # return node_face_connectivity
 
 
 @njit(cache=True)
 def _order_nodes(
     face_edges,
-    temp_face_edges,
+    temp_face,
     node_0,
     node_central,
     n_edges,
@@ -114,8 +103,9 @@ def _order_nodes(
 
     d_angles = np.zeros(n_edges, dtype=np.float64)
     d_angles[0] = 0.0
+    final_face = np.array([INT_FILL_VALUE for _ in range(n_edges)], dtype=INT_DTYPE)
     for j in range(1, n_edges):
-        _cur_face_temp_idx = temp_face_edges[j][0]
+        _cur_face_temp_idx = temp_face[j]
 
         if _cur_face_temp_idx is not INT_FILL_VALUE:
             sub = np.array(
@@ -142,8 +132,9 @@ def _order_nodes(
     d_current_angle = 0.0
     # face.set_node(0, face_temp[0])
 
-    _set_node(face_edges, 0, temp_face_edges[0][0])
+    # _set_node(face_edges, 0, temp_face[0])
 
+    final_face[0] = temp_face[0]
     for j in range(1, n_edges):
         ix_next_node = -1
         d_next_angle = 2.0 * np.pi
@@ -158,17 +149,10 @@ def _order_nodes(
 
         # face.set_node(j, face_temp[ix_next_node])
 
-        _set_node(face_edges, j, temp_face_edges[ix_next_node][0])
+        final_face[j] = temp_face[ix_next_node]
+        # _set_node(face_edges, j, temp_face[ix_next_node])
 
         d_current_angle = d_next_angle
+    final_face[n_edges] = temp_face[0]
 
-    return face_edges
-
-
-@njit(cache=True)
-def _set_node(edges, ix_local, ix_node):
-    n_edges = len(edges)
-    edges[ix_local][0] = ix_node
-
-    ix_prev = (ix_local + n_edges - 1) % n_edges
-    edges[ix_prev][1] = ix_node
+    return final_face
