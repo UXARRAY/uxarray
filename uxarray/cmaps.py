0c57250abb2b167fe24b391d58d31671f26ec29e
Aaron Zedwick <aazedwick@gmail.com>
2024-08-16 09:36:32 -0500

    Update cmaps.py

--- uxarray/cmaps.py
+++ uxarray/cmaps.py
@@ -4,11 +4,9 @@ from matplotlib.colors import LinearSegmentedColormap
 diverging = LinearSegmentedColormap.from_list(
     "diverging",
     (
-        (0.000, (0.698, 0.416, 0.137)),
-        (0.400, (0.867, 0.678, 0.498)),
+        (0.000, (0.016, 0.576, 0.565)),
         (0.500, (1.000, 1.000, 1.000)),
-        (0.600, (0.757, 0.859, 0.541)),
-        (1.000, (0.498, 0.584, 0.322)),
+        (1.000, (0.004, 0.400, 0.569)),
     ),
 )
 
@@ -18,9 +16,18 @@ sequential = LinearSegmentedColormap.from_list(
 )
 
 sequential_blue = LinearSegmentedColormap.from_list(
-    "blue", ((0.000, (0.004, 0.400, 0.569)), (1.000, (1.000, 1.000, 1.000)))
+    "sequential_blue", ((0.000, (1.000, 1.000, 1.000)), (1.000, (0.004, 0.400, 0.569)))
 )
 
 sequential_green = LinearSegmentedColormap.from_list(
-    "green", ((0.000, (0.016, 0.576, 0.565)), (1.000, (1.000, 1.000, 1.000)))
+    "sequential_green", ((0.000, (1.000, 1.000, 1.000)), (1.000, (0.016, 0.576, 0.565)))
+)
+
+sequential_green_blue = LinearSegmentedColormap.from_list(
+    "sequential_green_blue",
+    (
+        (0.000, (1.000, 1.000, 1.000)),
+        (0.500, (0.016, 0.576, 0.565)),
+        (1.000, (0.004, 0.400, 0.569)),
+    ),
 )
