880b928666ed7727cb56d6310077d74e3134664b
Philip Chmielowiec <philipchmielowiec@gmail.com>
2024-10-14 12:05:14 -0500

    add docstrings

--- uxarray/cross_sections/dataarray_accessor.py
+++ uxarray/cross_sections/dataarray_accessor.py
@@ -55,6 +55,34 @@ class UxDataArrayCrossSectionAccessor:
         return self.uxda.isel(n_face=faces)
 
     def constant_longitude(self, lon: float, method="fast"):
+        """Extracts a cross-section of the data array at a specified constant
+        longitude.
+
+        Parameters
+        ----------
+        lon : float
+            The longitude at which to extract the cross-section, in degrees.
+        method : str, optional
+            The internal method to use when identifying faces at the constant longitude.
+            Options are:
+            - 'fast': Uses a faster but potentially less accurate method for face identification.
+            - 'accurate': Uses a slower but more accurate method.
+            Default is 'fast'.
+
+        Raises
+        ------
+        ValueError
+            If no intersections are found at the specified longitude, a ValueError is raised.
+
+        Examples
+        --------
+        >>> uxda.constant_longitude_cross_section(lon=-15.5)
+
+        Notes
+        -----
+        The accuracy and performance of the function can be controlled using the `method` parameter.
+        For higher precision requreiments, consider using method='acurate'.
+        """
         faces = self.uxda.uxgrid.get_faces_at_constant_longitude(lon, method)
 
         return self.uxda.isel(n_face=faces)
--- uxarray/cross_sections/grid_accessor.py
+++ uxarray/cross_sections/grid_accessor.py
@@ -81,23 +81,23 @@ class GridCrossSectionAccessor:
 
     def constant_longitude(self, lon: float, return_face_indices=False, method="fast"):
         """Extracts a cross-section of the grid at a specified constant
-        latitude.
+        longitude.
 
         This method identifies and returns all faces (or grid elements) that intersect
-        with a given latitude. The returned cross-section can include either just the grid
+        with a given longitude. The returned cross-section can include either just the grid
         or both the grid elements and the corresponding face indices, depending
         on the `return_face_indices` parameter.
 
         Parameters
         ----------
-        lat : float
-            The latitude at which to extract the cross-section, in degrees.
+        lon : float
+            The longitude at which to extract the cross-section, in degrees.
         return_face_indices : bool, optional
-            If True, returns both the grid at the specified latitude and the indices
+            If True, returns both the grid at the specified longitude and the indices
             of the intersecting faces. If False, only the grid is returned.
             Default is False.
         method : str, optional
-            The internal method to use when identifying faces at the constant latitude.
+            The internal method to use when identifying faces at the constant longitude.
             Options are:
             - 'fast': Uses a faster but potentially less accurate method for face identification.
             - 'accurate': Uses a slower but more accurate method.
@@ -105,21 +105,21 @@ class GridCrossSectionAccessor:
 
         Returns
         -------
-        grid_at_constant_lat : Grid
-            The grid with faces that interesected at a given lattitude
+        grid_at_constant_lon : Grid
+            The grid with faces that interesected at a given longitudes
         faces : array, optional
-            The indices of the faces that intersect with the specified latitude. This is only
+            The indices of the faces that intersect with the specified longitude. This is only
             returned if `return_face_indices` is set to True.
 
         Raises
         ------
         ValueError
-            If no intersections are found at the specified latitude, a ValueError is raised.
+            If no intersections are found at the specified longitude, a ValueError is raised.
 
         Examples
         --------
-        >>> grid, indices = grid.cross_section.constant_latitude(lat=30.0, return_face_indices=True)
-        >>> grid = grid.cross_section.constant_latitude(lat=-15.5)
+        >>> grid, indices = grid.cross_section.constant_longitude(lat=0.0, return_face_indices=True)
+        >>> grid = grid.cross_section.constant_longitude(lat=20.0)
 
         Notes
         -----
