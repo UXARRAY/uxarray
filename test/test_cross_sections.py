4a4d66d1c7fa600891c13570eace7720701b1553
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-11-20 10:32:54 -0600

    remove non-bounds test case

--- test/test_cross_sections.py
+++ test/test_cross_sections.py
@@ -188,9 +188,3 @@ class TestExtremes:
         out2 = uxgrid.get_faces_at_constant_latitude(lat=20)
 
         nt.assert_array_equal(out1, out2)
-
-        out3 = uxgrid.get_faces_at_constant_latitude(lat=25)
-        out4 = uxgrid.get_faces_at_constant_latitude(lat=25)
-
-        assert len(out3) == 0
-        assert len(out4) == 1
