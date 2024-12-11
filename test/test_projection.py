958beadd27cbbc4809c53f0a8df5ae9be581c418
Rajeev Jain <rajeeja@gmail.com>
2024-12-10 21:08:06 -0600

    o Convert 4 more tests to pytest

--- test/test_projection.py
+++ test/test_projection.py
@@ -1,17 +1,16 @@
 import uxarray as ux
 import cartopy.crs as ccrs
-
 import os
 from pathlib import Path
-from unittest import TestCase
 
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 gridfile_geos_cs = current_path / "meshfiles" / "geos-cs" / "c12" / "test-c12.native.nc4"
 
+def test_geodataframe_projection():
+    """Test the projection of a GeoDataFrame."""
+    uxgrid = ux.open_grid(gridfile_geos_cs)
+    gdf = uxgrid.to_geodataframe(projection=ccrs.Robinson(), periodic_elements='exclude')
 
-class TestProjection(TestCase):
-    def test_geodataframe_projection(self):
-        uxgrid = ux.open_grid(gridfile_geos_cs)
-
-        gdf = uxgrid.to_geodataframe(projection=ccrs.Robinson(), periodic_elements='exclude')
+    # Example assertion to check if gdf is not None
+    assert gdf is not None
