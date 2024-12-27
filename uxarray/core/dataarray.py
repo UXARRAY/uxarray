463fe89119121ee15d8206b87921cc71e05bfbfa
Rachel Tam <116197442+rytam2@users.noreply.github.com>
2024-12-27 10:16:30 -0600

    Update uxarray/core/dataarray.py
    Co-authored-by: Aaron Zedwick <95507181+aaronzedwick@users.noreply.github.com>

--- uxarray/core/dataarray.py
+++ uxarray/core/dataarray.py
@@ -503,7 +503,7 @@ class UxDataArray(xr.DataArray):
         # compute the total weight
         total_weight = weights.sum()
 
-        # compute weighted mean #assumption on index of dimension (last one is geometry)
+        # compute the weighted mean, with an assumption on the index of dimension (last one is geometry)
         weighted_mean = (self * weights).sum(axis=-1) / total_weight
 
         # create a UxDataArray and return it
