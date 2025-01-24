e7be351f4c815a942f28d332d66eb26337fc247b
Philip Chmielowiec <philipchmielowiec@gmail.com>
2025-01-24 16:06:54 -0600

    update benchmark

--- benchmarks/mpas_dyamond.py
+++ benchmarks/mpas_dyamond.py
@@ -1,20 +1,18 @@
+import os
+
 from asv_runner.benchmarks.mark import skip_benchmark_if, timeout_class_at
 
 import uxarray as ux
-import os
 
-
-
-# TODO: ...
+# Paths to grid files on Glade
 grid_path_dict = {"30km": "/glade/campaign/cisl/vast/uxarray/data/dyamond/30km/grid.nc",
                   "15km": "/glade/campaign/cisl/vast/uxarray/data/dyamond/15km/grid.nc",
                   "7.5km": "/glade/campaign/cisl/vast/uxarray/data/dyamond/7.5km/grid.nc",
-                  "3.75km":"/glade/campaign/cisl/vast/uxarray/data/dyamond/3.75km/grid.nc"}
+                  "3.75km": "/glade/campaign/cisl/vast/uxarray/data/dyamond/3.75km/grid.nc"}
 
-# TODO: ...
-data_path_dict = ...
 
-# TODO: ...
+
+# Determines if all file paths exist and are accesible
 all_paths_exist = True
 for file_path in grid_path_dict.values():
     all_paths_exist = all_paths_exist and os.path.exists(file_path)
@@ -24,7 +22,7 @@ class BaseGridBenchmark:
     """Base class for Grid Benchmarks across the four supported resolutions
     (30km, 15km, 7.5km, 3.75km)"""
     param_names = ['resolution']
-    params = [['30km', '15km', '7.5km', '3.75km'],]
+    params = [['30km', '15km', '7.5km', '3.75km'], ]
 
     def setup(self, resolution, **kwargs):
         self.uxgrid = ux.open_grid(grid_path_dict[resolution])
@@ -33,8 +31,17 @@ class BaseGridBenchmark:
         del self.uxgrid
 
 
-@timeout_class_at(1000)
-class GeoDataFrameConversion(BaseGridBenchmark):
+class OpenGrid:
+    param_names = ['resolution']
+    params = [['30km', '15km', '7.5km', '3.75km'], ]
+
     @skip_benchmark_if(not all_paths_exist)
-    def time_to_geodataframe(self, resolution):
-        self.uxgrid.to_geodataframe(exclude_antimeridian=True)
+    def time_open_grid(self, resolution):
+        _ = ux.open_grid(grid_path_dict[resolution])
+
+
+@timeout_class_at(1200)
+class Bounds(BaseGridBenchmark):
+    @skip_benchmark_if(not all_paths_exist)
+    def time_bounds(self, resolution):
+        _ = self.uxgrid.bounds
