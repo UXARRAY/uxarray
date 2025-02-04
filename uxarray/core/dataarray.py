a31d3c1e743f03528adb4f91f71a5a72498d306c
Philip Chmielowiec <philipchmielowiec@gmail.com>
2025-02-03 18:25:41 -0600

    continue healpix reader, start user guide

--- uxarray/core/dataarray.py
+++ uxarray/core/dataarray.py
@@ -153,6 +153,10 @@ class UxDataArray(xr.DataArray):
         else:
             return None
 
+    def to_healpix(self):
+        # TODO:
+        pass
+
     def to_geodataframe(
         self,
         periodic_elements: Optional[str] = "exclude",
