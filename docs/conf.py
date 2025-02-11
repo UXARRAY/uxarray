293ce28ad5f5bc7b3f9221c8b6e184be05d2dac0
Rajeev Jain <rajeeja@gmail.com>
2025-02-10 23:43:25 -0600

    o Use new mathjax

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ mathjax_config = {
 }
 
 mathjax_path = (
-    "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+    'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
 )
 
 extlinks = {
