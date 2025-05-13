53e0bc4e5aa14ab49cf1d9277a3da9177d1b95e0
Philip Chmielowiec <philipchmielowiec@gmail.com>
2025-05-13 14:49:19 -0600

    update name

--- uxarray/core/dataarray.py
+++ uxarray/core/dataarray.py
@@ -491,7 +491,7 @@ class UxDataArray(xr.DataArray):
             uxgrid=self.uxgrid,
             dims=dims,
             coords={"latitudes": latitudes},
-            name=(self.name or "data") + "_zonal_mean",
+            name=self.name + "_zonal_mean" if self.name is not None else "zonal_mean",
             attrs={"zonal_mean": True},
         )
 
