0bf439ec22ab60116457051521bc9c89c8f1568e
Philip Chmielowiec <philipchmielowiec@gmail.com>
2025-05-21 15:54:36 -0500

    docstrings, rename to imshow

--- uxarray/plot/matplotlib.py
+++ uxarray/plot/matplotlib.py
@@ -2,19 +2,49 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import cartopy.crs as ccrs
+import matplotlib.pylab as plt
 import numpy as np
+from cartopy.mpl import geoaxes
 
 if TYPE_CHECKING:
     from uxarray import UxDataArray
 
 
-def plot(data: UxDataArray, ax=None, **kwargs):
+def imshow(
+    data: UxDataArray,
+    ax: geoaxes.GeoAxes = None,
+    *,
+    projection: ccrs.CRS = ccrs.PlateCarree(),
+    **kwargs,
+) -> plt.AxesImage:
+    """
+    Render a UxDataArray onto a GeoAxes by sampling the unstructured grid onto a screen-space grid.
+
+    If `ax` is None or not a GeoAxes, a new figure/GeoAxes will be created with the given projection.
+
+    Parameters
+    ----------
+    data
+        Unstructured-grid data array to render.
+    ax
+        A Cartopy GeoAxes (or None to create one).
+    projection
+        The map projection to use if creating a new axes.
+    **kwargs
+        Passed through to `ax.imshow` (e.g., cmap, vmin, vmax).
+
+    Returns
+    -------
+    The AxesImage returned by `ax.imshow`.
+    """
+    ax = _ensure_geoaxes(ax, projection=projection)
     fig = ax.get_figure()
     fig.canvas.draw()
-    res = _sample_grid(data, ax)
 
+    sampled = _nearest_neighbor_resample(data, ax)
     im = ax.imshow(
-        res,
+        sampled,
         origin="lower",
         extent=ax.get_xlim() + ax.get_ylim(),
         **kwargs,
@@ -23,37 +53,42 @@ def plot(data: UxDataArray, ax=None, **kwargs):
     return im
 
 
-def contour(data: UxDataArray, ax=None, **kwargs):
-    fig = ax.get_figure()
-    fig.canvas.draw()
-    res = _sample_grid(data, ax)
+def _ensure_geoaxes(
+    ax,
+    *,
+    projection,
+) -> geoaxes.GeoAxes:
+    """
+    Return a GeoAxes. If `ax` is already a GeoAxes, return it; otherwise
+    create a new figure & GeoAxes with the given projection.
+    """
+    if isinstance(ax, geoaxes.GeoAxes):
+        return ax
 
-    im = ax.contour(
-        res,
-        origin="lower",
-        extent=ax.get_xlim() + ax.get_ylim(),
-        **kwargs,
-    )
-
-    return im
-
-
-def contourf(data: UxDataArray, ax=None, **kwargs):
-    fig = ax.get_figure()
-    fig.canvas.draw()
-    res = _sample_grid(data, ax)
-
-    im = ax.contourf(
-        res,
-        origin="lower",
-        extent=ax.get_xlim() + ax.get_ylim(),
-        **kwargs,
-    )
-
-    return im
+    fig, ax = plt.subplots(subplot_kw={"projection": projection})
+    return ax
 
 
 def _get_points_from_axis(ax):
+    """
+    Compute 3D Cartesian coordinates for each pixel center in an Axes.
+
+    Parameters
+    ----------
+    ax : cartopy.mpl.geoaxes.GeoAxes
+        The map axes defining the projection and bounds for sampling.
+
+    Returns
+    -------
+    pts : numpy.ndarray, shape (N, 3)
+        Cartesian coordinates on the unit sphere corresponding to valid pixel centers.
+    valid : numpy.ndarray of bool, shape (ny, nx)
+        Boolean mask indicating which pixels correspond to finite longitude and latitude values.
+    nx : int
+        Number of columns (width) in the pixel grid.
+    ny : int
+        Number of rows (height) in the pixel grid.
+    """
     import cartopy.crs as ccrs
 
     x0, x1 = ax.get_xlim()
@@ -82,14 +117,32 @@ def _get_points_from_axis(ax):
     return pts, valid, nx, ny
 
 
-def _sample_grid(
+def _nearest_neighbor_resample(
     data: UxDataArray,
     ax=None,
 ):
+    """
+    Resample a UxDataArray onto screen-space grid using nearest-neighbor rasterization.
+
+    Parameters
+    ----------
+    data : UxDataArray
+        Unstructured-grid data to rasterize.
+    ax : matplotlib.axes.Axes
+        The target axes defining the sampling grid.
+
+    Returns
+    -------
+    res : numpy.ndarray, shape (ny, nx)
+        Array of resampled data values corresponding to each pixel.
+
+    Notes
+    -----
+    This function determines which face on the grid contains each pixel center and assigns
+    the data value of the nearest face to that pixel.
+    """
     pts, valid, nx, ny = _get_points_from_axis(ax)
     face_indices, counts = data.uxgrid.get_faces_containing_point(pts)
-
-    # TODO: instead of picking the first face, consider an average
 
     # pick the first face
     first_face = face_indices[:, 0]
