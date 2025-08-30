6cabd15a1f1fda0c6a30fdc4eda07c8351e25923
zmoon <zmoon92@gmail.com>
2025-08-30 01:29:42 -0500

    Add a few exclude patterns
    mainly for temporary notebook files that may be around

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,7 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
