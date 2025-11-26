89889e0abbe53d7ddac40dbf4712a721c7089091
Rajeev Jain <rajeeja@gmail.com>
2025-11-26 11:08:59 -0600

    o Trying to see if separating the dask/distributed installation with --no-deps before installing the other packages fixes the CI issue

--- ci/install-upstream.sh
+++ ci/install-upstream.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # adapted from https://github.com/pydata/xarray/blob/main/ci/install-upstream-wheels.sh
-
 # forcibly remove packages to avoid artifacts
 conda remove -y --force \
     cartopy \
@@ -14,14 +13,11 @@ conda remove -y --force \
     scipy \
     shapely \
     xarray
-
 pip uninstall -y \
     antimeridian \
     spatialpandas
-
 # conda list
 conda list
-
 # if available install from scientific-python nightly wheels
 python -m pip install \
     -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \
@@ -34,14 +30,17 @@ python -m pip install \
     scipy \
     geopandas \
     xarray
+# install dask and distributed first with --no-deps to avoid version conflicts
+python -m pip install \
+    --no-deps \
+    git+https://github.com/dask/dask.git \
+    git+https://github.com/dask/distributed.git
 
 # install rest from source
 python -m pip install \
     git+https://github.com/gadomski/antimeridian.git \
     git+https://github.com/SciTools/cartopy.git \
     git+https://github.com/holoviz/datashader.git \
-    git+https://github.com/dask/dask.git \
-    git+https://github.com/dask/distributed.git \
     git+https://github.com/holoviz/holoviews.git \
     git+https://github.com/shapely/shapely.git \
     git+https://github.com/holoviz/spatialpandas.git
