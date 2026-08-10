1210ae0d0b364728daca80a88502a6003454aa43
Sam Evans <47793072+Sevans711@users.noreply.github.com>
2026-08-10 10:41:09 -0400

    fix opt deps error hint tests typos
    also add string matching to test_check_requires_viz_and_geo in both the installed_with_geo and the installed_with_viz cases, and left a comment about why it wasn't added in the installed_with_no_opts case.

--- test_optional_deps/test_installed_with_no_opts.py
+++ test_optional_deps/test_installed_with_no_opts.py
@@ -20,17 +20,19 @@ def test_check_requires_no_opts():
 
 def test_check_requires_only_viz():
     """ensure failure for checks which should require viz optional dependencies"""
-    with pytest.raises(ImportError, match=r"pip install uxarray\[viz\]"):
+    with pytest.raises(ImportError, match=r'pip install "uxarray\[viz\]"'):
         check_requires_only_viz()
 
 
 def test_check_requires_only_geo():
     """ensure failure for checks which should require geo optional dependencies"""
-    with pytest.raises(ImportError, match=r"pip install uxarray\[geo\]"):
+    with pytest.raises(ImportError, match=r'pip install "uxarray\[geo\]"'):
         check_requires_only_geo()
 
 
 def test_check_requires_viz_and_geo():
     """ensure failure for checks which should require both viz and geo optional dependencies"""
     with pytest.raises(ImportError):
+        # ^no match "uxarray[geo,viz]" here; might crash in a viz-only or a geo-only method,
+        # even though the check itself ultimately requires both viz and geo.
         check_requires_viz_and_geo()
