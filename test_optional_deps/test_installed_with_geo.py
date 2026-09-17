c59d68ae4d906ca76f819b1c08a922ae3969f197
Sam Evans <47793072+Sevans711@users.noreply.github.com>
2026-09-17 19:24:26 -0400

    Add CI tests for optional dependencies (#1654)
    * add test_optional_deps files & ci commands
    
    * forgot pre-commit ruff formatting
    
    * fix ruff complaint about unused import
    
    * add healpix-sensitive optional deps test
    
    (maybe not necessary… but also trying to re-trigger CI jobs here, due to github actions downtime yesterday causing stalled jobs with no "rerun jobs" button available.)
    
    * fix optional deps test: cannot plot UxDataset
    
    * add test_optional_deps files & ci commands
    
    * forgot pre-commit ruff formatting
    
    * fix ruff complaint about unused import
    
    * add healpix-sensitive optional deps test
    
    (maybe not necessary… but also trying to re-trigger CI jobs here, due to github actions downtime yesterday causing stalled jobs with no "rerun jobs" button available.)
    
    * fix optional deps test: cannot plot UxDataset
    
    * fix important pyproject.toml typo: test not tests

---
+++ test_optional_deps/test_installed_with_geo.py
@@ -0,0 +1,35 @@
+"""
+Purpose: test expected behaviors when installed with only geo optional dependency.
+Tests should all pass if and only if installed accordingly, i.e. something like:
+    pip install ".[geo]"
+"""
+
+import pytest
+from _optional_deps_helpers import (
+    check_requires_no_opts,
+    check_requires_only_geo,
+    check_requires_only_viz,
+    check_requires_viz_and_geo,
+)
+
+
+def test_check_requires_no_opts():
+    """ensure success for checks which should not require any optional dependencies"""
+    check_requires_no_opts()
+
+
+def test_check_requires_only_viz():
+    """ensure failure for checks which should require viz optional dependencies"""
+    with pytest.raises(ImportError):
+        check_requires_only_viz()
+
+
+def test_check_requires_only_geo():
+    """ensure success for checks which should require geo optional dependencies"""
+    check_requires_only_geo()
+
+
+def test_check_requires_viz_and_geo():
+    """ensure failure for checks which should require both viz and geo optional dependencies"""
+    with pytest.raises(ImportError):
+        check_requires_viz_and_geo()
